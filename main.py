190d64c7e66a67277d2cd600ccaf1ee04806a78c
Mikunu <58949015+Mikunu@users.noreply.github.com>
2021-09-20 21:00:21 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import vk_api, vk
+import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-import datetime, random, json
 import json
+import random
 import requests
+import datetime
 import sqlite3
-import re
+import threading
+import time
+import schedule
+import sys
 
-def saymessage(message, chatid):
+def send_Message(message, chatid):
     vk.messages.send(
         key='265b9078df036d8b0bc08fa119f73547cb9e6fd9',
         server='https://lp.vk.com/wh203658568',
@@ -18,26 +22,7 @@ def saymessage(message, chatid):
     )
 
 
-def addlessons(gettedjson, cur, conn):
-    for timetable in gettedjson:
-        lesson = [
-            group,
-            timetable['auditorium'],
-            timetable['beginLesson'],
-            timetable['date'],
-            timetable['dayOfWeekString'],
-            timetable['discipline'],
-            timetable['endLesson'],
-            timetable['kindOfWork'],
-            timetable['lecturer'],
-            timetable['lecturerOid'],
-            timetable['stream'],
-            timetable['subGroup']]
-        cur.execute("INSERT INTO groups VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", lesson)
-        conn.commit()
-
-
-def DateToday(date):
+def dateToday(date):
     if date == 1:
         return 'Понедельник'
     elif date == 2:
@@ -54,25 +39,22 @@ def DateToday(date):
         return 'Воскресенье'
 
 
-def GetTimetable(group):
+def GetTimetable(group, selectedDay):
     url = 'https://rasp.omgtu.ru'
     # получаем json со всеми группами с такими или близкими названиями (ИВТ-201 и ЗИВТ-201)
     response = requests.get(f"{url}/api/search?term={group}&type=group")
     gettedjson = json.loads(response.text)
 
-    # print(gettedjson)
     # отсортировываем json чтобы получить id нужной группе, указанной в переменной group
     for label in gettedjson:
         if label['label'] == group:
-            print(label['id'])
             gettedid = str(label['id'])
 
     # получаем даты начала и конца недели
-    todaydate = datetime.datetime.today().isoweekday()
-    startdate = datetime.datetime.today() - datetime.timedelta(todaydate - 1)
-    finishdate = datetime.datetime.today() + datetime.timedelta(7 - todaydate)
-    # print(startdate.strftime('%Y.%m.%d'))
-    # print(finishdate.strftime('%Y.%m.%d'))
+    dayOfWeek = selectedDay.isoweekday()
+    startdate = selectedDay - datetime.timedelta(dayOfWeek - 1)
+    finishdate = selectedDay + datetime.timedelta(7 - dayOfWeek)
+
 
     # получаем json с расписанием группы
     response = requests.get(
@@ -84,128 +66,130 @@ def GetTimetable(group):
     return gettedjson
 
 
-def CreateTable(cur, conn):
-    cur.execute("""CREATE TABLE IF NOT EXISTS groups(
-        studgroup TEXT,
-    	auditorium TEXT,
-    	beginLesson TEXT,
-    	date TEXT,
-    	dayOfWeekString TEXT,
-    	discipline TEXT,
-    	endLesson TEXT,
-    	kindOfWork TEXT,
-    	lecturer TEXT,
-    	lecturerOid TEXT,
-    	stream TEXT,
-    	subGroup TEXT);""")
-    conn.commit()
+def getSingleLesson(time, selectedDay, gotJson, chatid):
+    entrymessage = f"Следующая пара скоро начнётся!\n---------------------------\n"
+    message = ''
+    urltext = ''
+    for result in gotJson:
+        if result["date"] == selectedDay.strftime('%Y.%m.%d'):
+            if result['beginLesson'] == time:
+                dbResult = cur.execute(f"SELECT * FROM disciplinelinks WHERE chatid={chatid}").fetchall()
+                if result['auditorium'].find('В.А') != -1:
+                    for elem in dbResult:
+                        if result['discipline'].find(elem[2]) != -1:
+                            urltext = elem[3]
+                if result['subGroup'] is None:
+                    message += f"{result['discipline']}\n{result['lecturer']}\n" \
+                               f"В {result['auditorium']} с {result['beginLesson']} по {result['endLesson']}\n{urltext}\n\n"
+                else:
+                    message += f"{result['discipline']}\n{result['lecturer']}\n" \
+                               f"В {result['auditorium']} с {result['beginLesson']} по {result['endLesson']} " \
+                               f"у {result['subGroup']}\n{urltext}\n\n"
+    entrymessage += message
+    return entrymessage
 
-
-def makeTable(cur, event, selectedDay):
-    cur.execute(f"SELECT groupname FROM chats WHERE chatid ='{str(event.chat_id)}';")
-    group = cur.fetchall()[0][0]
-    # print(group)
-
-    today = datetime.datetime.today()
-    if selectedDay is None:
-        selectedDay = today.strftime('%Y.%m.%d')
-    else:
-        # 04.09.21
-        if selectedDay.find(str(today.year)) == -1:
-            selectedDay += f'.{today.year}'
-            selectedDay = datetime.datetime.strptime(selectedDay, '%d.%m.%Y').strftime('%Y.%m.%d')
-            print(selectedDay)
-
-    print(selectedDay)
-    selectedDay = datetime.datetime.strptime(selectedDay, '%Y.%m.%d')
-    message = f"{DateToday(selectedDay.isoweekday())}\n-------------\n"
-    cur.execute(f"SELECT * FROM groups WHERE studgroup = '{group}' AND date = '{selectedDay.strftime('%Y.%m.%d')}';")
-    all_results = cur.fetchall()
-    # print(all_results)
-    if len(all_results) == 0:
-        if selectedDay is not None:
-            message = message + 'В этот день пар нет, отдыхайте'
-        else:
-            message = message + 'Сегодня пар нет, отдыхайте'
-    else:
-        for result in all_results:
-            '''
-            if (result[1] == 'В.А.33') or (result[1] == 'В.А.36'):  # result[1] = аудитория
-                cur.execute(f"SELECT * FROM teachers WHERE lecturerOid = '{result[9]}';")
-                url = cur.fetchone()
-                urltext = f"\n{url[1]}"
-            else:
-                url = ['', '']
-                urltext = ''
-            '''
+def makeTable(selectedDay, gotJson, chatid):
+    entrymessage = f"{dateToday(selectedDay.isoweekday())}, {selectedDay.strftime('%d.%m')}\n---------------------------\n"
+    message = ''
+    for result in gotJson:
+        if result["date"] == selectedDay.strftime('%Y.%m.%d'):
             urltext = ''
-            # print(message)
-            # print(result)
-            # result[5] = пара; result[8] = препод; result[1] = аудитория; result[2] = начало пары;
-            # result[6] = конец пары; result[11] = подгруппа
-            if result[11] is None:
-                message = message + f"{result[5]} ({result[8]}) в {result[1]} с {result[2]} по {result[6]}{urltext}\n\n"
-            else:
-                message = message + f"{result[5]} ({result[8]}) в {result[1]} с {result[2]} по {result[6]} у {result[11]}{urltext}\n\n"
+            dbResult = cur.execute(f"SELECT * FROM disciplinelinks WHERE chatid={chatid}").fetchall()
+            if result['auditorium'].find('В.А') != -1:
+                for elem in dbResult:
+                    if result['discipline'].find(elem[2]) != -1:
+                        urltext = elem[3]
 
-    return message
+            if result['subGroup'] is None:
+                message += f"{result['discipline']}\n{result['lecturer']}\n" \
+                           f"В {result['auditorium']} с {result['beginLesson']} по {result['endLesson']}\n{urltext}\n\n"
+            else:
+                message += f"{result['discipline']}\n{result['lecturer']}\n" \
+                           f"В {result['auditorium']} с {result['beginLesson']} по {result['endLesson']} " \
+                           f"у {result['subGroup']}\n{urltext}\n\n"
+
+    if len(message) == 0:
+        message = 'Сегодня пар нет, отдыхайте'
+
+    entrymessage += message
+    return entrymessage
+
+
+def notifier(cur):
+    time = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    time = time.strftime('%H:%M')
+    if time == '7:30':
+        result = cur.execute(f"SELECT * FROM chats").fetchall()
+        selectedDay = datetime.datetime.today()
+        for elem in result:
+            message = makeTable(selectedDay, GetTimetable(elem[1], selectedDay), elem[0])
+            send_Message(message, int(elem[0]))
+
+    times = ['08:00', '09:40', '11:20', '14:00', '15:40', '17:20', '19:00']
+    for timeElem in times:
+        if timeElem == time:
+            result = cur.execute(f"SELECT * FROM chats").fetchall()
+            selectedDay = datetime.datetime.today()
+            for elem in result:
+                message = getSingleLesson(time, selectedDay, GetTimetable(elem[1], selectedDay), elem[0])
+                if len(message) > 65:
+                    send_Message(message, int(elem[0]))
+
+def run_continuously(interval=1):
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return cease_continuous_run
 
 
 if __name__ == '__main__':
-
-    with open("loginstuff.json", "r") as read_file:
+    with open(r"C:\Users\rusla\PycharmProjects\TatarTechBot2.0\loginstuff.json", "r") as read_file:
         data = json.load(read_file)
-        print(data[0]['token'])
         vk_session = vk_api.VkApi(token=data[0]['token'])
         random.seed()
 
         longpoll = VkBotLongPoll(vk_session, data[0]['group_id'])
         vk = vk_session.get_api()
 
-    conn = sqlite3.connect('timetables.db')
+    conn = sqlite3.connect(r'C:\Users\rusla\PycharmProjects\TatarTechBot2.0\timetables.db', check_same_thread=False)
     cur = conn.cursor()
-    CreateTable(cur, conn)
     cur.execute("""CREATE TABLE IF NOT EXISTS chats(
-    chatid text,
-    groupname text
-    );""")
+        chatid text,
+        groupname text
+        );""")
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS disciplines(
-    chatid text,
-    disciplineOid text,
-    discipline text,
-    link text
-    );""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS disciplinelinks(
+        lineid int,
+        chatid text,
+        discipline text,
+        link text
+        );""")
     conn.commit()
+
+    schedule.every(1).minutes.do(notifier, cur)
+    stop_run_continuously = run_continuously()
 
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             if 'ТТ, расписание' in str(event):
                 if event.from_chat:
-                    command = event.message.text
-                    # print(command)
-                    # print(len(command))
-                    selectedDay = ''
-                    if 'завтра' in command:
-                        selectedDay = datetime.datetime.today() + datetime.timedelta(days=1)
-                        selectedDay = selectedDay.strftime('%Y.%m.%d')
-                        command = command.replace('завтра', selectedDay)
-                    # print(command)
-                    if len(command) > 14:
-                        selectedDay = command[15:]
-                    if len(selectedDay) == 0:
-                        selectedDay = None
-                    # print(selectedDay)
-                    message = makeTable(cur, event, selectedDay)
+                    selectedDay = datetime.datetime.today()
+                    if event.message.text[14:].find('завтра') != -1:
+                        selectedDay += datetime.timedelta(days=1)
+                        if event.message.text[14:].find('после') != -1:
+                            selectedDay += datetime.timedelta(days=1)
 
-                    vk.messages.send(
-                        key='4b413a03341032f343f6875f9a4e12e06a8d4a44',
-                        server='https://lp.vk.com/wh203814419',
-                        ts='4',
-                        random_id=get_random_id(),
-                        message=message,
-                        chat_id=event.chat_id
-                        )
+                    message = makeTable(selectedDay, GetTimetable('ИВТ-201', selectedDay), str(event.chat_id))
+                    send_Message(message, event.chat_id)
+
             elif 'TT, init' in str(event):
                 if event.from_chat:
                     # TT init ИВТ-202
@@ -214,30 +198,58 @@ if __name__ == '__main__':
                     cur.execute(f"SELECT COUNT ({event.chat_id}) from chats WHERE chatid = {event.chat_id}")
                     if cur.fetchall() == 0:
                         cur.execute("INSERT INTO chats VALUES(?, ?);", chat)
-                        saymessage(f'Беседа успешно инициализирована, ваша группа {group}', event.chat_id)
+                        send_Message(f'Беседа успешно инициализирована, ваша группа {group}', event.chat_id)
                         conn.commit()
-                        addlessons(GetTimetable(group), cur, conn)
                     else:
                         cur.execute(f"SELECT * from chats WHERE chatid = {event.chat_id}")
                         group = cur.fetchall()
-                        saymessage(f'Группа {group[0][1]} уже инициализирована!', event.chat_id)
+                        send_Message(f'Группа {group[0][1]} уже инициализирована!', event.chat_id)
 
-            elif 'TT, unit' in str(event):
+            elif 'TT, uninit' in str(event):
                 if event.from_chat:
-                    pass
+                    cur.execute(f"SELECT COUNT ({event.chat_id}) from chats WHERE chatid = {event.chat_id}")
+                    if cur.fetchall() == 0:
+                        send_Message(f'Беседа не инициализирована')
+                    else:
+                        cur.execute(f"DELETE FROM chats WHERE chatid = {event.chat_id}")
+                        conn.commit()
+                        send_Message(f'Связь беседы и группы разорвана')
+
             elif 'TT, добавить ссылку' in str(event):
                 if event.from_chat:
                     '''
                     Математика
                     http://b21523.vr.mirapolis.ru/mira/miravr/7544736809
                     При входе в систему необходимо указать свою фамилию и номер группы.
-                    
+
                     Дизайн интерфейса информационных систем
                     http://b21523.vr.mirapolis.ru/mira/miravr/6456350497
                     '''
-                    linkinfo = [event.chat_id]
-                    cur.execute("INSERT INTO lecturers VALUES (?, ?, ?);", )
+                    message = event.message.text[20:]
+                    if message.find('"') == -1:
+                        send_Message('Ошибка в вводе команды, отсутствуют кавычки,', event.chat_id)
+                    result = int(cur.execute(f"SELECT count(*) FROM disciplinelinks").fetchone()[0])
+                    print(result)
+                    if result == 0:
+                        linkid = 0
+                    else:
+                        linkid = result + 1
+
+                    disciplinename = message[message.find('"') + 1:message.rfind('"')]
+                    print(disciplinename)
+                    '''
+                    cur.execute(f"SELECT * FROM disciplinelinks WHERE discipline={disciplinename}")
+                    result = cur.fetchone()[0][0]
+                    print(result)
+                    if result is not None:
+                        send_Message('Данный предмет уже добавлен')
+                        '''
+                    link = message[message.rfind('"') + 2:]
+                    linkinfo = [linkid, event.chat_id, disciplinename, link]
+                    cur.execute("INSERT INTO disciplinelinks VALUES (?, ?, ?, ?);", linkinfo)
+                    conn.commit()
+                    send_Message(f'Ссылка {link} связана с {disciplinename}', event.chat_id)
 
             elif 'ТТ,' in str(event):
                 if event.from_chat:
-                    saymessage('Такой команды нет', event.chat_id)
+                    send_Message('Такой команды нет', event.chat_id)
